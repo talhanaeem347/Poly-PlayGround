f680cf05f42560973b1a0df00b8b3a9858fa579f
ali raza <34940@gcslahore.edu.pk>
2023-03-28 11:37:55 +0500

    Routing Implement

---
+++ lib/ui/auth/loginsignup/phoneverfication.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+
+import '../../../custom.dart';
+class PhoneVerifiaction extends StatelessWidget {
+  const PhoneVerifiaction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:  MyGradient(
+        child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back_ios, size: 20),
+                      onPressed: ()=>Navigator.pop(context),
+
+                      padding: EdgeInsets.symmetric(horizontal: 2),
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                CustomText(text: 'Verification code',color: Colors.black,),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    children: [
+
+                      const SizedBox(height: 16),
+                      InputField(hint: '----', icon: Icon(Icons.paste_sharp)),
+
+                       const SizedBox(height: 20),
+                      TextButton(
+                        onPressed: () {
+                          // Add resend functionality
+                        },
+                        child: Text('Resend!'.toString()),
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+
+                          onPrimary: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const CustomButton(
+                  path: '/home',
+                  child: Text('Vadidate'),
+                ),
+                const Spacer(),
+              ],
+            ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/ui/auth/loginsignup/signupwithphone.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+import '../../../custom.dart';
+
+class SignupWithPhone extends StatelessWidget {
+  const SignupWithPhone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [ Color(0xFF8A2D25),Color(0xFFFFFFFF)],
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+    Row(
+    children: [
+    IconButton(
+    icon: Icon(Icons.arrow_back_ios, size: 20),
+    onPressed: ()=>Navigator.pop(context),
+
+    padding: EdgeInsets.symmetric(horizontal: 2),
+    color: Colors.white,
+    ),
+    ],
+    ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50,vertical: 20),
+                  child: Container(
+                    height: 50,
+                    width: 230,
+                    margin: const EdgeInsets.only(left: 5, top: 10),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                    child: const Text(
+                      'Register your account',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'By email',
+                      style: TextStyle(
+                        height: 1.0,
+                        fontSize: 14,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      'By telephone',
+                      style: TextStyle(
+                        height: 1.0,
+                        fontSize: 14,
+                        color: Colors.white,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+
+                      const SizedBox(height: 16),
+                      Container(
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          color: Colors.white,
+                        ),
+                        child: const TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: 'PHONE NUMBER',
+                            prefixIcon: Icon(Icons.phone),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.all(7),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+                Padding(
+                  padding:const EdgeInsets.symmetric(horizontal: 120),
+                  child: Row(
+                    children: const [
+
+                      CustomButton(
+                        path: '/Phone_Verification',
+                        child: Text('Login'),
+
+                        ),
+
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+              ],
+            ),
+
+          ),
+        ),
+      ),
+    );
+  }
+}
